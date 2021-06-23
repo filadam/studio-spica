79028c162eaec696cb1aefd0d242b1cb89c36f58
Filip <filip.adamczyk@web-solutions.eu>
2021-06-23 15:52:10 +0200

    hotfix

--- web/wp-content/themes/spica/template-parts/front-page/front-video.php
+++ web/wp-content/themes/spica/template-parts/front-page/front-video.php
@@ -7,24 +7,24 @@
 
 ?>
 
-    <div class="row m-5 p-0 g-0 video">
-        <div class="overlay d-flex align-items-center justify-content-end">
-            <div class="col-xl-6 col">
-                <svg viewBox="0 0 1000 100">
-                <symbol id="s-text-2">
-                    <text text-anchor="middle" x="40%" y="50%" dy=".35em"><?php _e('Witaj w SPICA!') ?></text>
-                </symbol>
-                <use class="text-2" xlink:href="#s-text-2"></use> 
-                <use class="text-2" xlink:href="#s-text-2"></use>
-                <use class="text-2" xlink:href="#s-text-2"></use>
-                <use class="text-2" xlink:href="#s-text-2"></use>
-                <use class="text-2" xlink:href="#s-text-2"></use>
-                </svg>
-            </div>
-
+<div class="row m-5 p-0 g-0 video">
+    <div class="overlay d-flex align-items-center justify-content-end">
+        <div class="col-xl-6 col">
+            <svg viewBox="0 0 1000 100">
+            <symbol id="s-text-2">
+                <text text-anchor="middle" x="40%" y="50%" dy=".35em"><?php _e('Witaj w SPICA!') ?></text>
+            </symbol>
+            <use class="text-2" xlink:href="#s-text-2"></use> 
+            <use class="text-2" xlink:href="#s-text-2"></use>
+            <use class="text-2" xlink:href="#s-text-2"></use>
+            <use class="text-2" xlink:href="#s-text-2"></use>
+            <use class="text-2" xlink:href="#s-text-2"></use>
+            </svg>
         </div>
-        <video class='front-video' playsinline="playsinline" autoplay="" muted="" loop>
-            <source src="http://studiospica.local/wp-content/uploads/2021/06/stars.mp4" type="video/mp4">
-        </video>
-        <i class="bi bi-arrow-down-circle-fill video-mouse-overlay"></i>
+
     </div>
+    <video class='front-video' playsinline="playsinline" autoplay="" muted="" loop>
+        <source src="http://studiospica.local/wp-content/uploads/2021/06/stars.mp4" type="video/mp4">
+    </video>
+    <i class="bi bi-arrow-down-circle-fill video-mouse-overlay"></i>
+</div>
